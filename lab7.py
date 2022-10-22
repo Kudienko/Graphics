c6b2bdec8e8dfba950269f2e816954c8fbe2736a
PC_ilya <kudienko2016@mail.ru>
2022-10-22 17:31:34 +0300

    Computer commit

---
+++ lab7.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+data = pd.read_csv('hockey_players.csv', delimiter=',',encoding='utf-8')
+# лпаиаьиаьапь
+print(data)
+data = []
+pfpf = 2 +2
+print(pfpf)
+fddgfddggfdf = pfpf + 12
+print(fddgfddggfdf) # абракадабра
+
+print(2+2) #rfdkgfdmgdfkgdfgfmg dfkgf
+
