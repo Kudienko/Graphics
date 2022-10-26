44acc6504a431d742de28b13e959684c4872ad1f
PC_ilya <kudienko2016@mail.ru>
2022-10-26 08:07:23 +0300

    Computer commit

--- lab7.py
+++ lab7.py
@@ -15,8 +15,8 @@ with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         print(row)
-#
-# dict = {'Goalkeeper':'','Defender':'','Forward':''} #Круговая диаграммма
+#Круговая диаграмма
+# dict = {'Goalkeeper':'','Defender':'','Forward':''}
 # sum_Goalkeeper = 0
 # sum_Defender = 0
 # sum_Forward = 0
@@ -84,30 +84,107 @@ with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
 # plt.ylabel('Человек')
 # plt.show()
 # Гистограмма
-with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    year = []
-    for row in plots:
-        year.append(row[0])
-    year = set(year)
-    year.remove('year')
-    year_list = []
-    for i in range(1,len(year)):
-        year_list.append(i)
-    years_dict = dict.fromkeys(year,0)
+# with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
+#     plots = csv.reader(csvfile, delimiter=',')
+#     year = []
+#     for row in plots:
+#         year.append(row[0])
+#     year = set(year)
+#     year.remove('year')
+#     year_list = []
+#     for i in range(1,len(year)):
+#         year_list.append(i)
+#
+#     years_dict = dict.fromkeys(year,0)
+#
+# print(years_dict)
+# sum = 0
 
-print(years_dict)
-with open('hockey_players.csv', 'r', encoding='utf-8') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        for el in year:
-            if row[0] == el:
-                years_dict[el] += 1
-print(years_dict)
+# with open('hockey_players.csv', 'r', encoding='utf-8') as csvfile:
+#     plots = csv.reader(csvfile, delimiter=',')
+#     for row in plots:
+#         sum +=1
+#         for el in year:
+#             if row[0] == el:
+#                 years_dict[el] += 1
+#     sum = sum - 1
+# for x,y in years_dict.items():
+#     x = years_dict.get(x)
+#     result = int(sum) / int(x)
+#     years_dict[x] = float(result)
+#     print(result)
+# print(sum)
 # посчитать количесвтво игроков каждого года и общее количество игроков и поделить на количество
 # plt.title('Распределение хоккеистов по количеству участий в ЧМ')
 # plt.hist(year_list,edgecolor='black', bins =50)
 # plt.title('Распределение хоккеистов по количеству участий в ЧМ')
 # plt.xlabel('Количество ЧМ')
 # plt.ylabel('Доля')
+# plt.show()
+#График
+with open('hockey_players.csv', 'r', encoding='utf-8') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    years = []
+    dict = {'Goalkeeper':'','Defender':'','Forward':''}
+    height_Goalkeeper = []
+    height_Defender = []
+    height_Forward = []
+    for row in plots:
+        if row[0] and row[0] != 'year' not in years:
+            years.append(int(row[0]))
+        if row[4] == "G":
+            height_Goalkeeper.append(row[6])
+            dict['Goalkeeper'] = height_Goalkeeper
+        if row[4] == "D":
+            height_Defender.append(row[6])
+            dict['Defender'] = height_Defender
+        if row[4] == "F":
+            height_Forward.append(row[6])
+            dict['Forward'] = height_Forward
+    years =sorted(set(years))
+    years_dict = {}
+    for god in years:
+        years_dict[god] = []
+    with open('hockey_players.csv', 'r', encoding='utf-8') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        x = []
+        y = []
+        for row in plots:
+            x.append(row[0])
+            y.append(row[6])
+            for key,el in years_dict.items():
+                if row[0] == str(key):
+                    years_dict[key].append(int(row[6]))
+print(years_dict)
+
+
+# average = {key: round(sum(value)/len(value)) for key, value in years_dict.items()}
+#
+# print(average)
+# print(dict)
+# god = []
+# height = []
+# for x, y in average.items():
+#     god.append(x)
+#     height.append(y)
+# x = god
+# y = height
+plt.title('Тренды изменения роста игрока для каждой позиции')
+plt.xlabel('Год ЧМ')
+plt.ylabel('Рост(см.)')
+plt.plot(x,y)
+plt.legend(loc=4)
+plt.show()
+fig = plt.figure()
+#
+# ax_1 = fig.add_subplot(2, 2, 1)
+# ax_2 = fig.add_subplot(2, 2, 2)
+# ax_3 = fig.add_subplot(2, 2, 3)
+# ax_4 = fig.add_subplot(2, 2, 4)
+# fig.suptitle(f'Хоккейная статистика ЧM:{years}')
+# ax_1.set(title = 'ax_1', xticks=[], yticks=[])
+# ax_2.set(title = 'Тренды изменения роста игрока для каждой позиции', xticks=[], yticks=years)
+# ax_3.set(title = 'Распределение хоккеистов по месяцам рождения', xticks=[], yticks=[])
+# ax_4.set(title = 'Распределение позиций между хоккеистами', xticks=[], yticks=[])
+#
 # plt.show()
