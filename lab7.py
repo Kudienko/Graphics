ea3049e416011eb94135fee6564b2104e1195b71
kudienko_laptop <kudienko2016@mail.ru>
2022-10-27 10:24:29 +0300

    Ready work

--- lab7.py
+++ lab7.py
@@ -1,6 +1,6 @@
 import csv
 import re
-
+from statistics import mean
 import pandas as pd
 import numpy as np
 import matplotlib
@@ -15,31 +15,31 @@ with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         print(row)
-#Круговая диаграмма
-# dict = {'Goalkeeper':'','Defender':'','Forward':''}
-# sum_Goalkeeper = 0
-# sum_Defender = 0
-# sum_Forward = 0
-# with open('hockey_players.csv', 'r') as csvfile:
-#     lines = csv.reader(csvfile, delimiter=',')
-#     for row in lines:
-#         if row[4] == "G":
-#             sum_Goalkeeper += 1
-#             dict['Goalkeeper'] = sum_Goalkeeper
-#         if row[4] == "D":
-#             sum_Defender += 1
-#             dict['Defender'] = sum_Defender
-#         if row[4] == "F":
-#             sum_Forward += 1
-#             dict['Forward'] = sum_Forward
-# labels = []
-# sizes = []
-# for x, y in dict.items():
-#     labels.append(x)
-#     sizes.append(y)
-# plt.pie(sizes, labels=labels, autopct='%.2f%%')
-# plt.title('Распределение позиций между хоккеистами', fontsize=10)
-# plt.show()
+# Круговая диаграмма
+dict = {'Goalkeeper':'','Defender':'','Forward':''}
+sum_Goalkeeper = 0
+sum_Defender = 0
+sum_Forward = 0
+with open('hockey_players.csv', 'r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for row in lines:
+        if row[4] == "G":
+            sum_Goalkeeper += 1
+            dict['Goalkeeper'] = sum_Goalkeeper
+        if row[4] == "D":
+            sum_Defender += 1
+            dict['Defender'] = sum_Defender
+        if row[4] == "F":
+            sum_Forward += 1
+            dict['Forward'] = sum_Forward
+labels = []
+sizes = []
+for x, y in dict.items():
+    labels.append(x)
+    sizes.append(y)
+plt.pie(sizes, labels=labels, autopct='%.2f%%')
+plt.title('Распределение позиций между хоккеистами', fontsize=10)
+plt.show()
 # Столбчатая диаграмма
 # dict = {"Январь": 0,"Февраль": 0,"Март": 0,"Апрель": 0,"Май": 0,"Июнь": 0,"Июль": 0,"Август": 0,"Сентябрь": 0,"Октябрь": 0,"Ноябрь": 0,"Декабрь": 0,}
 # with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
@@ -84,107 +84,44 @@ with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
 # plt.ylabel('Человек')
 # plt.show()
 # Гистограмма
-# with open('hockey_players.csv', 'r',encoding='utf-8') as csvfile:
-#     plots = csv.reader(csvfile, delimiter=',')
-#     year = []
-#     for row in plots:
-#         year.append(row[0])
-#     year = set(year)
-#     year.remove('year')
-#     year_list = []
-#     for i in range(1,len(year)):
-#         year_list.append(i)
-#
-#     years_dict = dict.fromkeys(year,0)
-#
-# print(years_dict)
-# sum = 0
-
-# with open('hockey_players.csv', 'r', encoding='utf-8') as csvfile:
-#     plots = csv.reader(csvfile, delimiter=',')
-#     for row in plots:
-#         sum +=1
-#         for el in year:
-#             if row[0] == el:
-#                 years_dict[el] += 1
-#     sum = sum - 1
-# for x,y in years_dict.items():
-#     x = years_dict.get(x)
-#     result = int(sum) / int(x)
-#     years_dict[x] = float(result)
-#     print(result)
-# print(sum)
-# посчитать количесвтво игроков каждого года и общее количество игроков и поделить на количество
-# plt.title('Распределение хоккеистов по количеству участий в ЧМ')
-# plt.hist(year_list,edgecolor='black', bins =50)
-# plt.title('Распределение хоккеистов по количеству участий в ЧМ')
-# plt.xlabel('Количество ЧМ')
-# plt.ylabel('Доля')
+# data = pd.read_csv('hockey_players.csv', encoding="ISO-8859-1", header = 0)
+# data1 = data.copy()
+# data1.drop_duplicates(subset=['name'], keep='first', inplace=True)
+# x1=data['name'].value_counts()
+# data['name'].value_counts().hist(grid=False, color='r', figsize=(6,4), edgecolor='black')
+# plt.title(u'Распределение хоккеистов по количеству участий в ЧМ')
+# plt.xlabel("Количество ЧМ")
+# plt.ylabel("Доля")
 # plt.show()
 #График
-with open('hockey_players.csv', 'r', encoding='utf-8') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    years = []
-    dict = {'Goalkeeper':'','Defender':'','Forward':''}
-    height_Goalkeeper = []
-    height_Defender = []
-    height_Forward = []
-    for row in plots:
-        if row[0] and row[0] != 'year' not in years:
-            years.append(int(row[0]))
-        if row[4] == "G":
-            height_Goalkeeper.append(row[6])
-            dict['Goalkeeper'] = height_Goalkeeper
-        if row[4] == "D":
-            height_Defender.append(row[6])
-            dict['Defender'] = height_Defender
-        if row[4] == "F":
-            height_Forward.append(row[6])
-            dict['Forward'] = height_Forward
-    years =sorted(set(years))
-    years_dict = {}
-    for god in years:
-        years_dict[god] = []
-    with open('hockey_players.csv', 'r', encoding='utf-8') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        x = []
-        y = []
-        for row in plots:
-            x.append(row[0])
-            y.append(row[6])
-            for key,el in years_dict.items():
-                if row[0] == str(key):
-                    years_dict[key].append(int(row[6]))
-print(years_dict)
-
-
-# average = {key: round(sum(value)/len(value)) for key, value in years_dict.items()}
-#
-# print(average)
-# print(dict)
-# god = []
-# height = []
-# for x, y in average.items():
-#     god.append(x)
-#     height.append(y)
-# x = god
-# y = height
-plt.title('Тренды изменения роста игрока для каждой позиции')
-plt.xlabel('Год ЧМ')
-plt.ylabel('Рост(см.)')
-plt.plot(x,y)
-plt.legend(loc=4)
-plt.show()
-fig = plt.figure()
-#
-# ax_1 = fig.add_subplot(2, 2, 1)
-# ax_2 = fig.add_subplot(2, 2, 2)
-# ax_3 = fig.add_subplot(2, 2, 3)
-# ax_4 = fig.add_subplot(2, 2, 4)
-# fig.suptitle(f'Хоккейная статистика ЧM:{years}')
-# ax_1.set(title = 'ax_1', xticks=[], yticks=[])
-# ax_2.set(title = 'Тренды изменения роста игрока для каждой позиции', xticks=[], yticks=years)
-# ax_3.set(title = 'Распределение хоккеистов по месяцам рождения', xticks=[], yticks=[])
-# ax_4.set(title = 'Распределение позиций между хоккеистами', xticks=[], yticks=[])
-#
+# fig, ax2 = plt.subplots(figsize=(10,6))
+# ax2.set_title(u'Тренды изменения роста игрока для каждой позиции')
+# ax2.set_xlabel("Год ЧМ", fontsize=14)
+# ax2.set_ylabel("Рост (см.)", fontsize=14)
+# xx = pd.DataFrame()
+# data = pd.read_csv('hockey_players.csv', encoding="ISO-8859-1", header = 0)
+# data1 = data.copy()
+# data1.drop_duplicates(subset=['name'], keep='first', inplace=True)
+# x21=data[data['position']=='G'].groupby('year')['height'].mean()
+# x22=data[data['position']=='D'].groupby('year')['height'].mean()
+# x23=data[data['position']=='F'].groupby('year')['height'].mean()
+# xx['height'] = x21.copy()
+# xx['year'] = x21.index
+# xx2 = pd.DataFrame()
+# xx2['height'] = x22.copy()
+# xx2['year'] = x22.index
+# xx3 = pd.DataFrame()
+# xx3['height'] = x23.copy()
+# xx3['year'] = x23.index
+# z1 = np.polyfit(xx['year'], xx['height'], 1)
+# p1 = np.poly1d(z1)
+# z2 = np.polyfit(xx2['year'], xx2['height'], 1)
+# p2 = np.poly1d(z2)
+# z3 = np.polyfit(xx3['year'], xx3['height'], 1)
+# p3 = np.poly1d(z3)
+# ax2.plot(xx['year'], p1(xx['year']), linestyle='dashed')
+# ax2.plot(xx2['year'], p2(xx2['year']), linestyle='dashed')
+# ax2.plot(xx3['year'], p3(xx3['year']), linestyle='dashed')
+# ax2.legend(['Вратарь', 'Защитник', 'Нападающий'], fontsize=14,loc = 4)
+# ax2.tick_params(axis = 'both', labelsize = 14)
 # plt.show()
